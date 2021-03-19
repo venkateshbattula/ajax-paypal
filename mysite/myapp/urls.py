ca449e6268750153eff173cda3c056b33aa7136f
Venkatesh B <venkateshb@krinotek.com>
2021-03-19 18:06:42 +0530

    showproducts update

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', index, name='index'),
     path('showproducts', showproducts, name='showproducts'),
     path('addtocart/<int:id>', addtocart, name='addtocart'),
-    path('empty_cart', empty_cart, name='empty_cart'),
+    path('empty_cart/', empty_cart, name='empty_cart'),
     path('view_cart', view_cart, name='view_cart')
 
 ]
